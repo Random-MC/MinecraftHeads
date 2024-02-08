de2a2c2fffc54481641c41d170b21733b2ad2e37
towsifkafi <kafitowsif@gmail.com>
2024-02-09 00:13:02 +0600

    ✨ initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+### Fork of [MinecraftHeads](https://github.com/TheLuca98/MinecraftHeads) by [TheLuca98](https://github.com/TheLuca98)
+
+# MinecraftHeads
+
+This repository is effectively a mirror for the data made available by the [Minecraft-Heads.com public API](https://minecraft-heads.com/scripts/api.php). The data is provided here as a single JSON file and is updated weekly by a script that runs on GitHub Actions.
+
+## Purpose
+
+To allow developers to fetch the entire dataset with a single HTTP call and without putting an unnecessary burden on the Minecraft-Heads.com servers.
+
+## Downloads
+
+The file is available via CDN, courtesy of [jsDelivr](https://www.jsdelivr.com):
+
+```
+https://cdn.jsdelivr.net/gh/Random-MC/MinecraftHeads@master/heads.json
+```
+
+Or you can click here: **[direct download link](https://cdn.jsdelivr.net/gh/Random-MC/MinecraftHeads@master/heads.json)**
+
+## Usage
+
+The database is a standard JSON file. Each row in the file is a head.
+Example of a head object:
+```json
+{
+    "name": "@ Icon",
+    "uuid": "e978ac35-5f37-4f57-833c-5d6983afa18d",
+    "category": "alphabet",
+    "value": "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZGFkNDQ4OTkxMjAxNmYwZjkyOTVmMWY...",
+    "hash": "dad4489912016f0f9295f1f3a780f641c8a72eda11032643f7dbd619c101073a",
+    "tags": [
+        "Punctuation Mark",
+        "Icons (Ironblock)"
+    ]
+}
+```
+
+
+The value of the `hash` column is a portion of the skin URL: `https://textures.minecraft.net/texture/<HASH>`.
+
+The skin URL is used to generate the `textures` property (to include in the user profile), which is essentially the following JSON object, but base64-encoded:
+
+```json
+{ "textures": { "SKIN": { "url": "https://textures.minecraft.net/texture/<HASH>" } } }
+```
+
+Additional resources on this topic:
+
+- [Example of a user profile](https://sessionserver.mojang.com/session/minecraft/profile/853c80ef3c3749fdaa49938b674adae6)
+- [Mojang API on wiki.vg](https://wiki.vg/Mojang_API#UUID_to_Profile_and_Skin.2FCape)
+- [minotar/skin-spec on GitHub](https://github.com/minotar/skin-spec)
---
+++ heads.ts
@@ -0,0 +1,79 @@
+// Download the entire database of minecraft-heads.com as a JSON file
+import { parse } from "https://deno.land/std@0.113.0/flags/mod.ts";
+
+const Config = {
+  defaultFile: `heads.json`,
+  textureUrlPrefix: "http://textures.minecraft.net/texture/",
+  categories: [
+    "alphabet",
+    "animals",
+    "blocks",
+    "decoration",
+    "food-drinks",
+    "humans",
+    "humanoid",
+    "miscellaneous",
+    "monsters",
+    "plants",
+  ],
+};
+
+async function main(args: string[]) {
+  const flags = parse(args, {
+    default: { outFile: Config.defaultFile },
+    alias: { outFile: "o" },
+  });
+
+  Deno.createSync(flags.outFile);
+  console.log(`Data will be written to ${flags.outFile}`);
+
+  const data = [];
+  for (const category of Config.categories) {
+    console.log(`Downloading data for ${category}...`);
+    const res = await fetch(
+      `https://minecraft-heads.com/scripts/api.php?cat=${category}&tags=true`,
+    );
+    if (!res.ok) {
+      throw Error(`Received HTTP ${res.status} ${res.statusText} from API.`);
+    }
+    const jsonResult = await res.json();
+    if (jsonResult != null) {
+      for (const entry of jsonResult) {
+        data.push({
+          name: entry["name"],
+          uuid: entry["uuid"],
+          category: category,
+          value: entry["value"],
+          hash: extractHash(entry["value"]),
+          tags: formatTags(entry["tags"]),
+        });
+      }
+    }
+  }
+
+  console.log("Writing to file...");
+  Deno.writeTextFile(flags.outFile, JSON.stringify(data, null, 4));
+  console.log(`Done! ${data.length} entries written to ${flags.outFile}.`);
+}
+
+function extractHash(base64data: string) {
+  const obj = JSON.parse(atob(base64data));
+  const url = obj["textures"]["SKIN"]["url"];
+  if (!url.startsWith(Config.textureUrlPrefix)) {
+    throw Error("Unexpected URL format: " + url);
+  }
+  return url.replaceAll(Config.textureUrlPrefix, "");
+}
+
+function formatTags(tags: string | null) {
+  if (tags == null) {
+    return "";
+  } else {
+    return tags.split(",");
+  }
+}
+
+main(Deno.args).catch((e) => {
+  console.error(`[ERROR] ${e}`);
+  Deno.exit(1);
+});
